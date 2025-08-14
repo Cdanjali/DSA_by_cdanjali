11c39d39acb74bde1747d40797ab8d43a740ffaa
anjali verma <Anjaliverma8924@gmail.com>
2025-08-14 08:27:27 +0000

    Add LL.java (linked list code) and ignore .class files

---
+++ LL.java
@@ -0,0 +1,64 @@
+class LL{
+Node head;
+
+class Node{
+    String data;
+    Node next;
+
+    //consturctor
+    Node(String data)
+{
+this.data = data;
+this.next = null;
+
+}
+}
+//add - first 
+public void addFirst(String data){
+    Node newNode = new Node(data);
+    if(head == null){
+head =newNode;
+return;
+    }
+    newNode.next = head;
+    head = newNode;
+}
+
+//add - last
+public void addLast(String data){
+    Node newNode = new Node(data);
+    if(head == null){
+head =newNode;
+return;
+    }
+    Node currNode = head;
+    while(currNode.next != null){
+        currNode = currNode.next;
+    }
+    currNode.next = newNode;
+    
+}
+//print
+public void printList(){
+    if(head == null){
+        System.out.println("List is Empty");
+        return;
+    }
+    Node currNode = head;
+    while(currNode != null){
+        System.out.print(currNode.data + "-->");
+        currNode = currNode.next;
+    }
+   System.out.println("NULL");
+}
+
+ public static void main(String [] args){
+LL list = new LL();
+list.addFirst("This");
+list.addFirst("is");
+list.addLast("my");
+list.addLast("Code");
+list.printList();
+
+    }
+}
